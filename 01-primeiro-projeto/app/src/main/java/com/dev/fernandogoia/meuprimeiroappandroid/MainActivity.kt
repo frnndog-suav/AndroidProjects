044ebdd414c23259149479a08f54cf7222125b47
Fernando <frnnd.goia97@gmail.com>
2026-02-09 16:58:04 -0300

    feat: adding view binding to activity

--- 01-primeiro-projeto/app/src/main/java/com/dev/fernandogoia/meuprimeiroappandroid/MainActivity.kt
+++ 01-primeiro-projeto/app/src/main/java/com/dev/fernandogoia/meuprimeiroappandroid/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.dev.fernandogoia.meuprimeiroappandroid
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.dev.fernandogoia.meuprimeiroappandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        with(binding.tvHelloWorld){
+            text = "Teste"
+            textAlignment = TextView.TEXT_ALIGNMENT_CENTER
         }
     }
 }

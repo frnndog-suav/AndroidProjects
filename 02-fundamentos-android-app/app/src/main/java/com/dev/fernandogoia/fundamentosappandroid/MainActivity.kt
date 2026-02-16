57bc89d2bd46433ff5697251b935f6f2f17eb03d
Fernando <frnnd.goia97@gmail.com>
2026-02-16 11:48:15 -0300

    chore: navigation control between different fragments

--- 02-fundamentos-android-app/app/src/main/java/com/dev/fernandogoia/fundamentosappandroid/MainActivity.kt
+++ 02-fundamentos-android-app/app/src/main/java/com/dev/fernandogoia/fundamentosappandroid/MainActivity.kt
@@ -3,27 +3,57 @@ package com.dev.fernandogoia.fundamentosappandroid
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
+import com.dev.fernandogoia.fundamentosappandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val navController by lazy {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fcvMainContainer) as? NavHostFragment
+
+        navHostFragment?.navController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.fcvMainContainer) as NavHostFragment
+        binding.btnNextFragment.setOnClickListener {
+            navController?.currentDestination?.id.let {
+                when (it) {
+                    R.id.firstFragment -> {
+                        binding.btnNextFragment.text = "Voltar para primeiro framgment"
+                        navController?.navigate(R.id.action_firstFragment_to_secondFragment,
+                            bundleOf("first_arg" to arrayOf("1", "2", "3"))
+                        )
+                    }
 
-        val navController = navHostFragment.navController
+                    R.id.secondFragment -> {
+                        binding.btnNextFragment.text = "Ir para próxima tela"
+                        navController?.popBackStack()
+                    }
+                }
+            }
 
 
+        }
 
     }
+
+
 }
--- 02-fundamentos-android-app/app/src/main/java/com/dev/fernandogoia/fundamentosappandroid/SecondFragment.kt
+++ 02-fundamentos-android-app/app/src/main/java/com/dev/fernandogoia/fundamentosappandroid/SecondFragment.kt
@@ -1,17 +1,32 @@
 package com.dev.fernandogoia.fundamentosappandroid
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.dev.fernandogoia.fundamentosappandroid.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        val view = binding.root
         return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val firstArgument = arguments?.getStringArray("first_arg") ?: arrayOf()
+
+        Log.d("SecondFragment", "Argument ${firstArgument.joinToString()}")
     }
 }

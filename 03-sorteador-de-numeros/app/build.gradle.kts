ecb66d2f5c20ec9ede041167dbe44cf3db8135e1
Fernando <frnnd.goia97@gmail.com>
2026-02-26 17:11:43 -0300

    feat: implementing fragments screens

--- 03-sorteador-de-numeros/app/build.gradle.kts
+++ 03-sorteador-de-numeros/app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)

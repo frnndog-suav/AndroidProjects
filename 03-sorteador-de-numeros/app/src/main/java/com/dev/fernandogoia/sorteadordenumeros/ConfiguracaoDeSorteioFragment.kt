54ac24eb1896ce7645f36d3c22714abe6435cf6e
Fernando <frnnd.goia97@gmail.com>
2026-02-27 16:47:28 -0300

    feat: adding basic logic structure in fragments

--- 03-sorteador-de-numeros/app/src/main/java/com/dev/fernandogoia/sorteadordenumeros/ConfiguracaoDeSorteioFragment.kt
+++ 03-sorteador-de-numeros/app/src/main/java/com/dev/fernandogoia/sorteadordenumeros/ConfiguracaoDeSorteioFragment.kt
@@ -1,17 +1,58 @@
 package com.dev.fernandogoia.sorteadordenumeros
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources.getColorStateList
+import androidx.core.widget.addTextChangedListener
+import com.dev.fernandogoia.sorteadordenumeros.databinding.FragmentConfiguracaoDeSorteioBinding
 
 class ConfiguracaoDeSorteioFragment : Fragment() {
+
+    private var _binding: FragmentConfiguracaoDeSorteioBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_configuracao_de_sorteio, container, false)
+    ): View {
+        _binding = FragmentConfiguracaoDeSorteioBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            swtNotRepeatNumbers.setOnCheckedChangeListener { _, isChecked ->
+                swtNotRepeatNumbers.trackTintList =
+                    getColorStateList(
+                        requireContext(),
+                        if (isChecked) R.color.background_brand else R.color.content_tertiary
+                    )
+            }
+
+            etAmountNumbers.addTextChangedListener { text ->
+
+            }
+
+            etFromLimit.addTextChangedListener { text ->
+
+            }
+
+            etFinalLimit.addTextChangedListener { text ->
+
+            }
+
+        }
     }
 }
--- 03-sorteador-de-numeros/app/src/main/java/com/dev/fernandogoia/sorteadordenumeros/ResultadoSorteioFragment.kt
+++ 03-sorteador-de-numeros/app/src/main/java/com/dev/fernandogoia/sorteadordenumeros/ResultadoSorteioFragment.kt
@@ -5,14 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.dev.fernandogoia.sorteadordenumeros.databinding.FragmentResultadoSorteioBinding
+import kotlin.random.Random
 
 class ResultadoSorteioFragment : Fragment() {
+
+    private var _binding: FragmentResultadoSorteioBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_resultado_sorteio, container, false)
+    ): View {
+        _binding = FragmentResultadoSorteioBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            tvDrawNumber.text = getString(R.string.numero_do_sorteio, "10")
+
+            gerarTextoDeNumeroSorteado()
+        }
+    }
+
+    fun FragmentResultadoSorteioBinding.gerarTextoDeNumeroSorteado() {
+        val numeroSorteadoTextView = TextView(requireContext()).apply {
+            id = View.generateViewId()
+            text = Random.nextInt().toString()
+            setTextAppearance(R.style.TextAppearance_RobotoMono_Overline)
+            textSize = 48f
+            setTextColor(ContextCompat.getColor(requireContext(), R.color.content_brand))
+        }
+
+        root.addView(numeroSorteadoTextView)
+        flowResultNumbersHelper.referencedIds =
+            flowResultNumbersHelper.referencedIds.plus(numeroSorteadoTextView.id)
     }
 
 }

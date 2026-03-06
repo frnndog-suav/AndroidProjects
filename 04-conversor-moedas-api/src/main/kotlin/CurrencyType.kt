f903f78ec50d093584789cf62cc3fedb32e32ded
Fernando <frnnd.goia97@gmail.com>
2026-03-06 17:04:46 -0300

    feat: implementing routes

---
+++ 04-conversor-moedas-api/src/main/kotlin/CurrencyType.kt
@@ -0,0 +1,53 @@
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CurrencyType(
+    val name: String,
+    val acronym: String,
+    val symbol: String,
+    val countryFlagImageUrl: String
+)
+
+@Serializable
+data class CurrencyTypeResult(
+    val values: List<CurrencyType>
+)
+
+val currencyTypes = listOf(
+    CurrencyType(
+        acronym = "USD",
+        name = "Dólar Americano",
+        symbol = "$",
+        countryFlagImageUrl = "https://flagcdn.com/us.svg"
+    ),      // USA
+    CurrencyType(
+        acronym = "EUR",
+        name = "Euro",
+        symbol = "€",
+        countryFlagImageUrl = "https://flagcdn.com/eu.svg"
+    ),                 // European Union
+    CurrencyType(
+        acronym = "JPY",
+        name = "Iene Japonês",
+        symbol = "¥",
+        countryFlagImageUrl = "https://flagcdn.com/jp.svg"
+    ),         // Japan
+    CurrencyType(
+        acronym = "GBP",
+        name = "Libra Esterlina",
+        symbol = "£",
+        countryFlagImageUrl = "https://flagcdn.com/gb.svg"
+    ),      // United Kingdom
+    CurrencyType(
+        acronym = "CNY",
+        name = "Yuan Chinês",
+        symbol = "¥",
+        countryFlagImageUrl = "https://flagcdn.com/cn.svg"
+    ),          // China
+    CurrencyType(
+        acronym = "BRL",
+        name = "Real Brasileiro",
+        symbol = "R$",
+        countryFlagImageUrl = "https://flagcdn.com/br.svg"
+    ),     // Brazil
+)
--- 04-conversor-moedas-api/src/main/kotlin/Routing.kt
+++ 04-conversor-moedas-api/src/main/kotlin/Routing.kt
@@ -1,15 +1,35 @@
 package com.fernandogoia
 
-import io.ktor.serialization.kotlinx.json.*
+import CurrencyTypeResult
+import currencyTypes
+import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        get("/currency_types") {
+            call.respond(
+                CurrencyTypeResult(
+                    values = currencyTypes
+                )
+            )
         }
+
+        get("/exchange_rate/{from}/{to}") {
+            val from = call.parameters["from"]?.uppercase() ?: return@get call.respondText(
+                status = HttpStatusCode.BadRequest,
+                text = "Não foi possível obter o acrônimo da moeda atual."
+            )
+            val to = call.parameters["to"]?.uppercase() ?: return@get call.respondText(
+                status = HttpStatusCode.BadRequest,
+                text = "Não foi possível obter o acrônimo da moeda atual."
+            )
+
+
+        }
+
+
     }
 }
